df24acc807f2e9d91862f411f43209e53ce8c131
chandrasen <chandrasendr1992@gmail.com>
2017-06-04 18:40:00 +0200

    create_lottert_numbers method will create a set which need not to be containing 6 numbers, bcz the random number generator may generate the same numbers when a for loop runs for 6 times and since the set does not return the duplicates we may end up having less than 6 numbers

--- lottery.py
+++ lottery.py
@@ -1,3 +1,6 @@
+import random
+
+# User can pick 6 numbers
 def get_player_numbers():
     number_csv = input("PLease enter any 6 numbers between 0 and 20 sepetated by commas: ")
     # Now create a set of integers from this number_csv
@@ -7,4 +10,13 @@ def get_player_numbers():
     integer_set = {int(number) for number in number_list}
     return integer_set
 
-print(get_player_numbers())
+# Lottery calculates 6 random numbes (between 1 and 20)
+def create_lottery_numbers():
+    values = set() # We cannot initialize an empty set like so: {}
+    for index in range(6): # Range in [0, 1, 2, 3, 4, 5]
+        values.add(random.randint(1, 20))
+        return values
+    
+    
+
+print(create_lottery_numbers())
