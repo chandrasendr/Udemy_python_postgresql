7294279c78df5a53e24e6bc94616aa1848d29e2a
chandrasen <chandrasendr1992@gmail.com>
2017-06-04 19:10:15 +0200

    This script takes 6 csv numbers as user input, generates 6 random numbers, and compares the random numbers with user input numbers to give the lottery prize of the user

--- lottery.py
+++ lottery.py
@@ -1,6 +1,18 @@
 import random
 
-# User can pick 6 numbers
+# Step:3 Then we match the user numbers to the lottery numbers
+def menu():
+    # Ask player for numbers
+    player_number = get_player_numbers()
+    
+    # Calculate lottery numbers
+    lottery_number = create_lottery_numbers()
+    
+    # Print out the winnings
+    winnings = player_number.intersection(lottery_number)
+    print("You won ${}!".format(100 ** len(winnings)))
+
+# Step:1 User can pick 6 numbers
 def get_player_numbers():
     number_csv = input("PLease enter any 6 numbers between 0 and 20 sepetated by commas: ")
     # Now create a set of integers from this number_csv
@@ -10,13 +22,13 @@ def get_player_numbers():
     integer_set = {int(number) for number in number_list}
     return integer_set
 
-# Lottery calculates 6 random numbes (between 1 and 20)
+# Step:2 Lottery calculates 6 random numbes (between 1 and 20)
 def create_lottery_numbers():
     values = set() # We cannot initialize an empty set like so: {}
     while len(values) < 6: # Range in [0, 1, 2, 3, 4, 5]
         values.add(random.randint(1, 20))
     return values
-    
-    
 
-print(create_lottery_numbers())
+    
+menu()
+
