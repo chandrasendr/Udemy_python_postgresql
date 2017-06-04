f75ee213ec5b9ddd7ca9627d141e2a7a7433492e
chandrasen <chandrasendr1992@gmail.com>
2017-06-04 15:34:56 +0200

    Use of return statement

--- magic_number.py
+++ magic_number.py
@@ -3,16 +3,16 @@ import random
 def ask_user_and_check_number():
     user_number = int(input("Enter a number between 0 and 9: "))
     if user_number in magic_number:
-        print('You got it right')
+        return 'You got it right'
     if user_number not in magic_number:
-        print('You did not quite get it')
+        return 'You did not quite get it'
 
 magic_number = [random.randint(0, 9), random.randint(0, 9)]
 
 def run_programme_x_time(chances):
     for i in range(chances): # range chances is [0, 1, 2]
         print("This is {} attempt". format(i + 1))
-        ask_user_and_check_number()
+        print(ask_user_and_check_number())
 
 chances = int(input("Enter the number of chances: "))
 run_programme_x_time(chances)
