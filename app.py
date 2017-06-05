4544b7944c28f31283d068159de63f46e26adb77
chandrasen <chandrasendr1992@gmail.com>
2017-06-05 18:07:58 +0200

    This is the complete code which asks the user to enter the name, marks of the students to get the average marks of the students

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+student_list = []
+
+
 def create_student():
     # Ask user for students name
     # Create the dictionary in the format {'name': '<student_name>', 'marks': []}
@@ -11,12 +14,10 @@ def create_student():
     return student_data
 
 
-
 def add_mark(student, mark):
     # Append a mark to the student dictionary
     student['marks'].append(mark)
     return None
-
 
 
 def calculate_avg_marks(student):
@@ -28,3 +29,39 @@ def calculate_avg_marks(student):
         return 0
     total = sum(student['marks'])
     return total / number
+
+
+def print_student_details(student):
+    # Print out a string that tells user important informationabout this student
+    print('{}, average mark: {}'. format(student['name'],
+                                         calculate_avg_marks(student)))
+
+
+def print_student_list(students):
+    for i, student in enumerate(students):
+        print("ID: {}". format(i))
+        print_student_details(student)
+
+
+def menu():
+    # Adding a student (to student_list)
+    # Add a mark to student
+    # Print a list of students
+    # Exit the application
+    selection = input("Enter 'p' to print the student list, "
+                      "'s' to add new student, "
+                      "'a' to add mark to student, or "
+                      "'q' to quit")
+    if selection == 'p':
+        print_student_list(student_list)
+    elif selection == 's':
+        student_list.append(create_student())
+    elif selection == 'a':
+        student_id = int(input("Enter a student ID to add a mark to: "))
+        student = student_list[student_id]
+        new_mark = int(input("Enter the new mark to be added: "))
+        add_mark(student, new_mark)
+
+menu()
+
+
