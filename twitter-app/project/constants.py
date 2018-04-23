e0a94cef8d004e57d9858b505502349924023b90
Chandrasen <chandrasen91@gmail.com>
2018-04-23 20:04:41 +0200

    Update constants.py

--- twitter-app/project/constants.py
+++ twitter-app/project/constants.py
@@ -1,5 +1,5 @@
-CONSUMER_KEY = '8tGQVmFEduqtoIQYvZ2YoIHeH'
-CONSUMER_SECRET = 'AZS35zcpGOtBbwBBk61LZ4WvqLRiMzKo6qIkYmIgBRcJJWIdAR'
+CONSUMER_KEY = 'XXXXXXXXXXX'
+CONSUMER_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXX'
 
 REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
 ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
